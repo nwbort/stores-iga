f172842ef191fa782e60f88204d4f534f20559ed
Nick Twort <nick.twort@houstonkemp.com>
2025-11-06 09:11:09 +1100

    Add headers

--- process_stores.py
+++ process_stores.py
@@ -9,14 +9,26 @@ from bs4 import BeautifulSoup
 SITEMAP_FILES_PATTERN = "iga.com.au-stores-sitemap*.xml.txt"
 OUTPUT_FILE = "stores.json"
 
-# It's good practice to set a user-agent to identify your scraper
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Connection": "keep-alive",
+    "Host": "www.iga.com.au",
+    "Sec-Ch-Ua": '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"Windows"',
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
 }
 
 # --- Concurrency Limiter ---
 # To avoid overwhelming the server, we limit the number of concurrent requests.
-CONCURRENCY_LIMIT = 50
+CONCURRENCY_LIMIT = 10
 semaphore = asyncio.Semaphore(CONCURRENCY_LIMIT)
 # ---------------------------
 
